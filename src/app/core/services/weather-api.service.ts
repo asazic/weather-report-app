9cf8f821a49ce3abcac58cfb26d0170236246542
unknown <amel_sazic@hotmail.com>
2022-10-14 09:40:04 +0200

    Code cleanup

--- src/app/core/services/weather-api.service.ts
+++ src/app/core/services/weather-api.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from "rxjs";
-import { DailyWeatherComponent } from "src/app/components/shared/daily-weather/daily-weather.component";
+import { map, Observable } from "rxjs";
 import { environment } from 'src/environments/environment';
 import { DailyWeather, ForecastResponse, WeatherResponse } from "../model/weather-data";
 
